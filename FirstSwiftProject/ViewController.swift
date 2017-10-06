2a2406dd6d051f25dfcf2afd9ff4bfc59fe503cd
ConnorOLeary <themidlvlboss@gmail.com>
2017-10-06 09:22:08 -0600

    Make an amazing app that changes color

--- FirstSwiftProject/ViewController.swift
+++ FirstSwiftProject/ViewController.swift
@@ -18,8 +18,25 @@ class ViewController: UIViewController //Colon shows the relationship between ob
     }
 
     @IBAction func firstMethod(_ sender: UIButton) {
-        		
+        if(firstButton.backgroundColor == .orange)
+        {
+            firstButton.backgroundColor = .black
+        }else{
+            firstButton.backgroundColor = .orange
+        }
+        view.backgroundColor = createRandomColor()
     }
+    
+    private func createRandomColor() -> UIColor {
+        let newColor :UIColor
+        let redAmount = CGFloat (Double(arc4random_uniform(256))/255.00)
+        let greenAmount = CGFloat (Double(arc4random_uniform(256))/255.00)
+        let blueAmount = CGFloat (Double(arc4random_uniform(256))/255.00)
+        newColor = UIColor(red: redAmount, green: greenAmount, blue: blueAmount, alpha: CGFloat(1.0))
+        
+        return newColor
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
